a036a276193e4e8fa1171aa093e35a4775f0a787
Heethesh Vhavle <heethesh@gmail.com>
2019-10-28 18:07:32 -0400

    IPM setup for rc car

--- delta_perception_rc_car/scripts/ipm_node.py
+++ delta_perception_rc_car/scripts/ipm_node.py
@@ -51,39 +51,12 @@ ipm_fps = FPSLogger('IPM')
 def process(image_msg, image_pub, vis=True, **kwargs):
     # Read image message
     img = message_to_cv2(image_msg)
-    # seg_img = message_to_cv2(seg_msg)
-
-    # Preprocess
-    # img = increase_brightness(img)
 
     # Run segmentation
     ipm_img = ipm.transform_image(img.copy())
 
-    # Extract the lane points
-    # lane_points_top, lane_points_bot = [], []
-    # for lane in lane_array.lanes:
-    #     # Top
-    #     lane_point = ipm.transform_points_to_px([lane.xtop, lane.ytop])
-    #     lane_points_top.append(tuple(lane_point[:2].astype('int').tolist()))
-
-    #     # Bottom
-    #     lane_point = ipm.transform_points_to_px([lane.xbot, lane.ybot])
-    #     lane_points_bot.append(tuple(lane_point[:2].astype('int').tolist()))
-
-    # Project segmentation image to IPM
-    # seg_img = ipm.transform_image(seg_img)
-
     # Visualize and publish image message
-    if vis:
-        # Display lane points
-        # for top, bot in zip(lane_points_top, lane_points_bot):
-        #     cv2.line(ipm_img, top, bot, (0, 255, 0), 2)
-
-        # Overlay segmentation
-        # ipm_img = cv2.addWeighted(ipm_img, 1.0, seg_img, 0.6, 0)
-
-        # Publish the output
-        cv2_to_message(ipm_img, image_pub)
+    if vis: cv2_to_message(ipm_img, image_pub)
 
 
 def callback(image_msg, image_pub, **kwargs):
@@ -100,19 +73,12 @@ def run(**kwargs):
     rospy.init_node('main', anonymous=True)
     rospy.loginfo('Current PID: [%d]' % os.getpid())
 
-    # Setup models
-    # ipm.setup()
-
     # Handle params and topics
-    image_color = rospy.get_param('~image_color', '/carla/ego_vehicle/camera/rgb/front/image_color')
-    # lane_marking = rospy.get_param('~lane_marking', '/delta/perception/lane/markings')
-    # segmentation = rospy.get_param('~segmentation', '/delta/perception/segmentation/image')
-    output_image = rospy.get_param('~output_image', '/delta/perception/ipm/image')
+    image_color = rospy.get_param('~image_color', '/camera/image_color_rect')
+    output_image = rospy.get_param('~output_image', '/delta/perception_rc_car/ipm/image')
 
     # Display params and topics
     rospy.loginfo('Image topic: %s' % image_color)
-    # rospy.loginfo('Lane Marking topic: %s' % lane_marking)
-    # rospy.loginfo('Segmentation topic: %s' % segmentation)
     rospy.loginfo('Output topic: %s' % output_image)
 
     # Publish output topic
@@ -120,8 +86,6 @@ def run(**kwargs):
 
     # Subscribe to topics
     image_sub = message_filters.Subscriber(image_color, Image)
-    # seg_sub = message_filters.Subscriber(segmentation, Image)
-    # lane_sub = message_filters.Subscriber(lane_marking, LaneMarkingArray)
 
     # Synchronize the topics by time
     ats = message_filters.ApproximateTimeSynchronizer([image_sub], queue_size=1, slop=0.1)
